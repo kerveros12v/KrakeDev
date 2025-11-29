f8f71d7cbda581975d849495b05c6ab3a1ca5848
kerveros12v <kerveros12v@aol.com>
2025-11-29 12:19:24 -0500

    modificacion de la funcion saludar

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar=function () {
+    let cmpImagenSaludo
+    cmpImagenSaludo=document.getElementById("imgSaludo")
+    cmpImagenSaludo.src="./imagenes/img2.gif"
     console.log("ingresa a saludar")
 }
