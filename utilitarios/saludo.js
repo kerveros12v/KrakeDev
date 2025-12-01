79a1e73f7e175a8f930f397211521bee573a5941
Vinueza Pozo Gabriel Fernando <kerveros12v@aol.com>
2025-12-01 09:25:38 -0500

    Función recuperarInt y función recuperarFoat

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -1,6 +1,8 @@
 saludar=function () {
     let nombre=recuperarTexto("txtNombre")
     let apellido=recuperarTexto("txtApellido")
+    let edad=recuperarInt("txtEdad")
+    let altura=recuperarFloat("txtEstatura")
 }
 recuperarTexto = function (idcomponente) {
     let componente;
@@ -9,3 +11,13 @@ recuperarTexto = function (idcomponente) {
     valorIngresado = componente.value;
     return valorIngresado;
 } 
+recuperarInt = function (idcomponente) {
+    let valorCaja = recuperarTexto(idcomponente);
+    let valorEntero = parseInt(valorCaja);
+    return valorEntero;
+}
+recuperarFloat = function (idcomponente) {
+    let valorCaja = recuperarTexto(idcomponente);
+    let valorFloat = parseFloat(valorCaja);
+    return valorFloat;
+}
