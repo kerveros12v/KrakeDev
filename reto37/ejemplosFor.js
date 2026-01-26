33d72bb0c21f6b881b073e2458526f1fa480f739
Vinueza Pozo Gabriel Fernando <kerveros12v@aol.com>
2026-01-26 18:34:53 -0500

    reto 37

---
+++ reto37/ejemplosFor.js
@@ -0,0 +1,41 @@
+mostrarNumeros = function () {
+    console.log("antes del for");
+    for (let i = 0; i < 4; i++) {
+        console.log(i);
+    }
+    console.log("despues del for")
+}
+mostrarNumeros2 = function () {
+    console.log("antes del for");
+    for (let indice = 0; indice <=5; indice++) {
+        console.log(indice);
+    }
+    console.log("despues del for")
+}
+mostrarPares = function () {
+    console.log("antes del for");
+    for (let x = 0; x <=10; x+=2) {
+        console.log(x);
+    }
+    console.log("despues del for")
+}
+mostrarInverso = function () {
+    console.log("antes del for");
+    for (let i = 10; i>0; i--) {
+        console.log(i);
+    }
+    console.log("despues del for")
+}
+hackearNasaPelis=function () {
+    for (let porcentaje = 0; porcentaje <=100; porcentaje+=10) {
+        console.log("Hackeando nasa "+porcentaje+" %");
+        
+    }
+}
+mostrarImpares = function () {
+    console.log("antes del for");
+    for (let x = 1; x <=21; x+=2) {
+        console.log(x);
+    }
+    console.log("despues del for")
+}
