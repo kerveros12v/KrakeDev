82c0b0cfb93473630242927387c30616a5724fd9
Vinueza Pozo Gabriel Fernando <kerveros12v@aol.com>
2026-01-31 10:37:40 -0500

    reto 47

---
+++ reto47/1-roles/roles/roles.js
@@ -0,0 +1,112 @@
+let empleados = [
+    { cedula: "1714616123", nombre: "John", apellido: "Cena", sueldo: 500.0 },
+    { cedula: "0914632123", nombre: "Luisa", apellido: "Gonzalez", sueldo: 900.0 }
+]
+let esNuevo = false;
+mostrarOpcionEmpleado = function () {
+    mostrarComponente("divEmpleado");
+    ocultarComponente("divRol");
+    ocultarComponente("divResumen");
+    mostrarImagen("imgEmpleado", "./imagenes/empleado.jpg");
+    deshabilitarComponente("txtCedula");
+    deshabilitarComponente("txtNombre");
+    deshabilitarComponente("txtApellido");
+    deshabilitarComponente("txtSueldo");
+    deshabilitarComponente("btnGuardar");
+    mostrarEmpleados();
+}
+mostrarOpcionRol = function () {
+    ocultarComponente("divEmpleado");
+    mostrarComponente("divRol");
+    ocultarComponente("divResumen");
+    mostrarImagen("imgRol", "./imagenes/rol.jpg");
+}
+mostrarOpcionResumen = function () {
+    ocultarComponente("divEmpleado");
+    ocultarComponente("divRol");
+    mostrarComponente("divResumen");
+    mostrarImagen("imgResumen", "./imagenes/resumen.jpg");
+}
+mostrarEmpleados = function () {
+    let contTabla = "<table><tr><th>CEDULA</th><th>NOMBRE</th><th>APELLIDO</th><th>SUELDO</th></tr>";
+    let elementoEmpleado;
+    for (let i = 0; i < empleados.length; i++) {
+        elementoEmpleado = empleados[i];
+        contTabla += "<tr><td>" + elementoEmpleado.cedula + "</td><td>" + elementoEmpleado.nombre + "</td><td>" + elementoEmpleado.apellido + "</td><td>" + elementoEmpleado.sueldo + "</td></tr>"
+    }
+    contTabla += "</table>";
+    document.getElementById("tablaEmpleados").innerHTML = contTabla;
+}
+ejecutarNuevo = function () {
+    habilitarComponente("txtCedula");
+    habilitarComponente("txtNombre");
+    habilitarComponente("txtApellido");
+    habilitarComponente("txtSueldo");
+    habilitarComponente("btnGuardar");
+    esNuevo = true;
+}
+buscarEmpleado = function (cedula) {
+    let elementoEmpleado;
+    for (let i = 0; i < empleados.length; i++) {
+        elementoEmpleado = empleados[i];
+        if (elementoEmpleado.cedula == cedula) break;
+        else elementoEmpleado = null;
+    }
+    return elementoEmpleado;
+}
+agregarEmpleado = function (empleado) {
+    let elementoEmpleado = buscarEmpleado(empleado.cedula);
+
+    if (elementoEmpleado == null) {
+        empleados.push(empleado);
+        return true;
+    }
+    return false;
+}
+guardar = function () {
+    let valorCedula = recuperarTexto("txtCedula");
+    let valorNombre = recuperarTexto("txtNombre");
+    let valorApellido = recuperarTexto("txtApellido");
+    let valorSueldo = recuperarFloat("txtSueldo");
+    let validadoCedula = false;
+     let validadonombre = false;
+      let validadoApellido = false;
+     let validadoSueldo= false;
+    let resultado;
+    if (longitudTextoValido(valorCedula, 10, 10) == true) {
+        for (let i = 0; i < valorCedula.length; i++) {
+            if (esDigito(valorCedula[i]) == true) validadoCedula = true;
+            else validadoCedula = false;
+        }
+    } else validadoCedula = false;
+    if (longitudTextoValido(valorNombre, 3, 20) == true) {
+        for (let i = 0; i < valorNombre.length; i++) {
+            if (esMayuscula(valorNombre[i]) == true) validadonombre = true;
+            else validadonombre = false;
+        }
+    } else validadonombre = false;
+    if (longitudTextoValido(valorApellido, 3, 20) == true) {
+        for (let i = 0; i < valorApellido.length; i++) {
+            if (esMayuscula(valorApellido[i]) == true) validadoApellido = true;
+            else validadoApellido = false;
+        }
+    }
+    else validadoApellido = false;
+    if (valorValido(valorSueldo, 400, 5000) == true) validadoSueldo = true;
+    else validadoSueldo = false;
+    if (validadoCedula == true &&validadonombre== true&& validadoApellido== true&&validadoSueldo== true) {
+        if (esNuevo == true) {
+            let empleadoNuevo = { cedula: valorCedula, nombre: valorNombre, apellido: valorApellido, sueldo: valorSueldo }
+            resultado = agregarEmpleado(empleadoNuevo);
+        }
+        if (resultado == true) {
+            alert("EMPLEADO GUARDADO CORRECTAMENTE");
+            mostrarEmpleados();
+        } else {
+            alert("YA EXISTE UN EMPLEADO CON LA CEDULA " + valorCedula);
+        }
+    }else{
+        alert("LOS DATOS INGRESADOS NO CUMPLEN CON LOS PARAMETROS");
+    }
+
+}
---
+++ reto47/1-roles/roles/utilitarios.js
@@ -0,0 +1,74 @@
+
+mostrarImagen = function (idComponente, rutaImagen) {
+    let componente;
+    componente = document.getElementById(idComponente);
+    componente.src = rutaImagen;
+}
+mostrarTexto = function (idComponente, mensaje) {
+    let componente;
+    componente = document.getElementById(idComponente);
+    componente.innerText = mensaje;
+}
+mostrarTextoEnCaja = function (idComponente, mensaje) {
+    let componente;
+    componente = document.getElementById(idComponente);
+    componente.value = mensaje;
+}
+
+recuperarTexto = function (idComponente) {
+    let componente;
+    let valorIngresado;
+    componente = document.getElementById(idComponente);
+    valorIngresado = componente.value;
+    return valorIngresado;
+}
+
+recuperarInt = function (idComponente) {
+    let valorCaja = recuperarTexto(idComponente);
+    let valorEntero = parseInt(valorCaja);
+    return valorEntero;
+}
+
+recuperarFloat = function (idComponente) {
+    let valorCaja = recuperarTexto(idComponente);
+    let valorFlotante = parseFloat(valorCaja);
+    return valorFlotante;
+}
+
+mostrarComponente = function (idComponente) {
+    document.getElementById(idComponente).style.display = "block";
+}
+
+ocultarComponente = function (idComponente) {
+    document.getElementById(idComponente).style.display = "none";
+}
+
+deshabilitarComponente = function (idComponente) {
+    document.getElementById(idComponente).disabled = true;
+}
+
+habilitarComponente = function (idComponente) {
+    document.getElementById(idComponente).disabled = false;
+}
+longitudTextoValido = function (texto, minimo, maximo) {
+    let tamanio = texto.length;
+    if (tamanio >= minimo && tamanio <= maximo) return true;
+    return false;
+
+}
+valorValido = function (numero, minimo, maximo) {
+
+    if (numero >= minimo && numero <= maximo) return true;
+    return false;
+
+}
+esMayuscula = function (caracter) {
+    let codigo = caracter.charCodeAt(0);
+    if (codigo >= 64 && codigo <= 90) return true;
+    return false;
+}
+esDigito = function (caracter) {
+    let codigo = caracter.charCodeAt(0);
+    if (codigo >= 48 && codigo <= 57) return true;
+    return false;
+} 
